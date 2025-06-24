11b4de8d81110df2042ef673d0fa1b9c51b1fa80
pedronogr <pedronogueiram2001@gmail.com>
2025-06-24 20:11:37 -0300

    Corrige navbar ausente na página do Flandor

--- app/produtos/flandor/page.tsx
+++ app/produtos/flandor/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import SchemaOrg from '../../components/SchemaOrg';
 import ProductClient from './ProductClient';
 
-export default function Page() {
-  return <ProductClient />;
+export default function FlandorPage() {
+  return (
+    <>
+      <Header />
+      <SchemaOrg />
+      <ProductClient />
+      <Footer />
+    </>
+  );
 } 
